5416e698821a9a5c262681fff16dd04c21bb2c65
Michael Chudinsky (New) <michaelchudinsky@MacBook-Air-2.local>
2026-05-26 22:11:11 +0200

    Update onboarding help button copy

--- app/page.tsx
+++ app/page.tsx
@@ -2280,7 +2280,7 @@ async function updateWeight() {
                   onClick={openOnboarding}
                   className="help-pulse mt-3 rounded-full border border-green-500/30 bg-green-950/30 px-4 py-2 text-xs font-black text-green-300"
                 >
-                  Як це працює?
+                  Як все тут працює?
                 </button>
               </div>
 
